02dbda9ec52e356f0628d5d74974186b5fcf6845
mirco <mircomannino1997@gmail.com>
2020-06-17 22:47:16 +0200

    Add ReLU activation

--- include/activationFunction.h
+++ include/activationFunction.h
@@ -17,11 +17,18 @@ class SigmoidActivation : public ActivationFunction {
     virtual double computeActivationPrime(const double a);
 };
 
+/* ReLU activation */
+class ReLUActivation : public ActivationFunction {
+    virtual double computeActivation(const double a);
+    virtual double computeActivationPrime(const double a);
+};
+
 /******************************** FACTORY *************************************/
 class ActivationFunctionFactory {
 public:
     static ActivationFunction* Build(const std::string type) {
         if(type == "sigmoid") return new SigmoidActivation();
+        if(type == "relu") return new ReLUActivation();
         return NULL;
     }
 };
--- src/activationFunction.cc
+++ src/activationFunction.cc
@@ -7,3 +7,13 @@ double SigmoidActivation::computeActivation(const double a) {
 double SigmoidActivation::computeActivationPrime(const double a) {
     return (this->computeActivation(a) * (1 - this->computeActivation(a)));
 }
+
+/* ReLU activation */
+double ReLUActivation::computeActivation(const double a) {
+    if(a < 0) return 0;
+    return a;
+}
+double ReLUActivation::computeActivationPrime(const double a) {
+    if(a <= 0) return 0;
+    return 1;
+}
